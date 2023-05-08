78c0219fe4c230ba5e59e19d4084f29f4d416cfb
Dongmin Lee <kid33629@gmail.com>
2023-05-08 11:28:10 +0900

    enhance/remove-package-install (#7)
    * enhance/remove-package-install
    
    * Resolve github action error
    
    * Resolve import issue
    
    * Apply PR comments

--- src/day_1/ch1_env_usage/frozenlake_human_play.py
+++ src/day_1/ch1_env_usage/frozenlake_human_play.py
@@ -31,33 +31,32 @@ def main() -> None:
 
     env = gym.make("FrozenLake8x8-v1", render_mode="human")
 
-    for _ in range(3):
-        done = False
-        obs, _ = env.reset()
+    done = False
+    obs, _ = env.reset()
 
-        while not done:
-            # Choose an action from keyboard
-            key = inkey()
-            if key not in arrow_keys.keys():
-                print("Game aborted!")
-                break
+    while not done:
+        # Choose an action from keyboard
+        key = inkey()
+        if key not in arrow_keys.keys():
+            print("Game aborted!")
+            break
 
-            action = arrow_keys[key]
-            next_obs, reward, done, truncated, _ = env.step(action)
+        action = arrow_keys[key]
+        next_obs, reward, done, truncated, _ = env.step(action)
 
-            # Show the board after action
-            env.render()
+        # Show the board after action
+        env.render()
 
-            print(
-                f"observation: {obs} | "
-                f"action: {action} | "
-                f"reward: {reward} | "
-                f"next_observation: {next_obs} | "
-                f"done: {done} | "
-                f"truncated: {truncated}\n",
-            )
+        print(
+            f"observation: {obs} | "
+            f"action: {action} | "
+            f"reward: {reward} | "
+            f"next_observation: {next_obs} | "
+            f"done: {done} | "
+            f"truncated: {truncated}\n",
+        )
 
-            obs = next_obs
+        obs = next_obs
 
 
 if __name__ == "__main__":
--- src/day_1/ch2_dp/policy_iteration/policy_iteration_solution.py
+++ src/day_1/ch2_dp/policy_iteration/policy_iteration_solution.py
@@ -4,7 +4,7 @@ from typing import Any
 
 import gym
 import numpy as np
-from day_1.ch2_dp.visualization import visualize_results
+from visualization import visualize_results
 
 
 def policy_evaluation(
